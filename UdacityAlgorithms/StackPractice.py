0c718e25e9f4a4c91f726312ff7a83472fc83e4e
Anna <asharkova0804@gmail.com>
2020-06-18 20:59:58 -0400

    Algorithm practice

--- UdacityAlgorithms/StackPractice.py
+++ UdacityAlgorithms/StackPractice.py
@@ -45,11 +45,11 @@ class Stack(object):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
+        """Push (add) a new element onto the top of the stack"""
         self.ll.insert_first(new_element)
 
     def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
+        """Pop (remove) the first element off the top of the stack and return it"""
         return self.ll.delete_first()
 
 
